527b526d5f16a2db142b854488807a191f4759fc
Robert Franken <hokasu@gmail.com>
2009-08-04 18:36:10 +0100

    patient view basics complete

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,22 @@
 class Patient < ActiveRecord::Base
   has_many :admissions
   validates_presence_of :patient_id
+  #lastadmission = self.admissions.last #why doesn't this work?
+
+  def fullname
+    firstname + " " + middlename + " " + surname
+  end
+
+  def has_allergies?
+    !allergies.size==0
+  end
+
+  def has_sensitivities?
+    !sensitivities.size==0
+  end
+
+  def inpatient?
+    DateTime.now < self.admissions.last.depdate && DateTime.now > self.admissions.last.admdate
+  end
+
 end
