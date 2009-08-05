d8e34f79c99a521bd20b517a4fff16ddd7f0c1e7
Robert Franken <hokasu@gmail.com>
2009-08-05 17:46:45 +0100

    edit admnotes issues

--- app/controllers/admnotes_controller.rb
+++ app/controllers/admnotes_controller.rb
@@ -9,41 +9,40 @@ class AdmnotesController < ApplicationController
     @admnote = @admission.admnotes.new
   end
 
+  
+  def show
+    @admnote = Admnote.find(params[:id])
+  end
+
   def create
     @admnote = Admnote.new(params[:admnote])
     @admnote.admission = Admission.find(params[:admission_id]) #setting foreign key
    
 
-    respond_to do |format|
       if @admnote.save
         flash[:notice] = 'Admission was successfully created.'
         redirect_to admission_admnotes_path (@admnote.admission)
       else
         #???TODO
       end
-    end
   end
     
   def update
     @admnote = Admnote.find(params[:id])
 
-    respond_to do |format|
       if @admnote.update_attributes(params[:admnote])
         flash[:notice] = 'Admission note was successfully updated.'
         redirect_to(@admnote) 
       else
         #???
       end
-    end
   end
 
   def destroy
     @admnote = Admnote.find(params[:id])
     @admnote.destroy
 
-    respond_to do |format|
       redirect_to(admnote_url)
-    end
   end
 
 end
