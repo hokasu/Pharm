23afe6d64952c58c8e597cc993ddef33ee4eb138
Robert Franken <hokasu@gmail.com>
2009-08-05 21:03:01 +0100

    all admnotes?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'patients' #rf
   map.resources :patients, :has_many => :admissions
- map.resources :admissions, :has_many => :admnotes, :shallow => true #rf
+ map.resources :admissions, :has_many => :admnotes
+ map.resources :admnotes
 #sets up the routes for index, create, update and destroy actions of admnotes controller
  #so /admnotes, /admnotes/new, POST /admnotes/:id, PUT /admnotes/:id, and DELETE /admnotes/:id
 # map.admit 'patients/:patient_id/admit', :controller => 'admissions', :action => 'admit'
