635de7a81043effd0ff8f22a1f4ae2e69e073eda
Robert Franken <hokasu@gmail.com>
2009-08-26 15:55:20 +0100

    dummy

--- app/models/product.rb
+++ app/models/product.rb
@@ -23,4 +23,31 @@ class Product < ActiveRecord::Base
     return s
   end
 
+  def agents_description
+    a=agents
+    s = ""
+    agents.each do |a|
+      s = s + a.name + " " + a.strength.to_s + a.measure
+      unless a.id == agents.last.id
+        s = s + "/"
+      end
+    end
+    return s
+  end
+
+  def brand_description
+    a = agents
+    s = brand + " "
+    agents.each do |a|
+      s = s + a.strength.to_s + a.measure
+      unless a.id == agents.last.id
+        s = s + "/"
+      end
+    end
+    s = s + " " + form.form + "s"
+    return s
+  end
+
+
+
 end
