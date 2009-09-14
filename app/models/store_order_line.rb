26ae99094f7af8c611a043f5cb30441c4e5dbfb3
Robert Franken <hokasu@gmail.com>
2009-09-14 19:31:05 +0100

    store orders

--- app/models/store_order_line.rb
+++ app/models/store_order_line.rb
@@ -1,4 +1,9 @@
 class StoreOrderLine < ActiveRecord::Base
   belongs_to :product_store
   belongs_to :store_order
+
+  def description
+    quantity.to_s + " x " + product_store.product.description
+  end
+
 end
